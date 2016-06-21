15769d4dc7c32684f86041795d2cfbd633c5c028
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2016-06-21 18:13:18 +0300

    Symlink script

---
+++ symlink.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Symlinking dotfiles"
+
+# need to be in the home
+cd
+
+ln -s dots/.gitconfig .gitconfig
+ln -s dots/.nethackrc .nethackrc
+ln -s dots/.tmux.conf .tmux.conf
+ln -s dots/.vimrc .vimrc
+ln -s dots/.zshrc .zshrc
+
+echo "Done"
